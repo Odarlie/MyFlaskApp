c4055812676372eadcdd8b296ca247354a2d9961
odarlie <odarlie@gmail.com>
2017-06-11 22:39:02 +0300

    my index.html page for my homepage

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 
@@ -9,4 +11,4 @@ app = Flask(__name__)
 
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug = True)
